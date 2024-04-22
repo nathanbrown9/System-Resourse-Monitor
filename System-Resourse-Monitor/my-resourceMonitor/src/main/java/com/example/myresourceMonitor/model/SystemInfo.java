5a177ef6d277c9947da690ee2499562d5ee7f72e
InesMustedanagic <140466158+InesMustedanagic@users.noreply.github.com>
2024-04-22 15:57:40 -0400

    Add files via upload
    added bandwidth and correct output for other functions as well. trying to see if html and css changes work w it

---
+++ System-Resourse-Monitor/my-resourceMonitor/src/main/java/com/example/myresourceMonitor/model/SystemInfo.java
@@ -0,0 +1,70 @@
+package com.example.myresourceMonitor.model;
+import org.springframework.stereotype.Component;
+import java.lang.management.ManagementFactory;
+import com.sun.management.OperatingSystemMXBean;
+import java.text.DecimalFormat;
+
+@Component
+public class SystemInfo {
+
+    public static OperatingSystemMXBean osInfo = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+
+    public static int getProcessors() {
+        return osInfo.getAvailableProcessors();
+    }
+
+    public static String getSystemLoadAverage() {
+        double returned = osInfo.getSystemLoadAverage();
+        String output = Double.toString(returned);
+        return output;
+    }
+
+    public static double getProcessCpuLoad() {
+        return osInfo.getCpuLoad();
+    }
+
+    public static String getFreeMemory() {
+        long freemem = osInfo.getFreeMemorySize();
+        String str = Long.toString(freemem);
+        return str;
+    }
+
+    public static String getAllocatedMemory(){
+        long allocmem = osInfo.getCommittedVirtualMemorySize();
+        String str = Long.toString(allocmem);
+        return str;
+    }
+
+    // correct
+    public static String[] getMemory(){
+        String[] returnMem = new String[2];
+        returnMem[0] = getFreeMemory();
+        returnMem[1] = getAllocatedMemory();
+        return returnMem;
+    }
+
+    public static String[] getIO(){
+        String[] returned = new String[2];
+        double Input = osInfo.getProcessCpuLoad();
+        double Output = osInfo.getSystemLoadAverage();
+        String str1 = String.valueOf(Input);
+        String str2 = String.valueOf(Output);
+        returned[0] = str1;
+        returned[1] = str2;
+        return returned;
+    }
+
+    public static String getNetworkIncoming() {
+        String returned = SystemInfo.getNetworkIncoming();
+        return returned;
+    }
+
+    public static String getMemoryUsageGB() {
+        long totalMemoryBytes = osInfo.getTotalMemorySize();
+        long freeMemoryBytes = osInfo.getFreeMemorySize();
+        long usedMemoryBytes = totalMemoryBytes - freeMemoryBytes;
+        double usedMemoryGB = (double) usedMemoryBytes / (1024 * 1024 * 1024);
+        DecimalFormat df = new DecimalFormat("#.##");
+        return df.format(usedMemoryGB);
+    }
+}
