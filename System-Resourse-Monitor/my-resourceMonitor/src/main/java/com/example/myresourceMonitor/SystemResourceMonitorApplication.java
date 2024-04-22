1c8a06910c9b10a8ac1baae5f5010a47924e0bb4
InesMustedanagic <140466158+InesMustedanagic@users.noreply.github.com>
2024-04-22 16:21:57 -0400

    Add files via upload

---
+++ System-Resourse-Monitor/my-resourceMonitor/src/main/java/com/example/myresourceMonitor/SystemResourceMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.example.myresourceMonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemResourceMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemResourceMonitorApplication.class, args);
+	}
+}
+
